45f2b26e2e4593386e0b9ccd1cbb241f99857693
Andy Pérez <andy.perez.amado@gmail.com>
2026-02-16 22:07:05 -0600

    FIXED: ModuleNotFoundError: No module named 'module'

--- src/felt/__init__.py
+++ src/felt/__init__.py
@@ -5,9 +5,11 @@
 #
 # Fabric configuration file.
 
-from importlib import import_module, util
+from importlib import util
+from importlib.machinery import ModuleSpec
 from os import environ
 from pathlib import Path
+import sys
 from types import ModuleType
 from typing import Any
 
@@ -17,22 +19,42 @@ CONFIG_PATH: Path = Path(
 	environ.get("XDG_CONFIG_HOME", Path.home()/".config")
 )/"felt"
 
-def load_config() -> dict[str, Any]:
+def get_module_path(name: str) -> Path:
 	"""
-	Loads config from $XDG_CONFIG_HOME/fabric/config.py
+	Resolves the module path inside the XDG_CONFIG_HOME folder.
+	"""
+	if name == "config":
+		return CONFIG_PATH/"config.py"
+	mod_path: Path = CONFIG_PATH/name
+	mod_path.mkdir(parents=True, exist_ok=True)
+	return mod_path/"main.py"
+
+def import_config_module(name: str) -> ModuleType:
+	"""
+	Imports and returns the module inside XDG_CONFIG_HOME.
+	"""
+	path: Path = get_module_path(name)
+	path.touch(exist_ok=True)
+	module_name: str = name
+	spec: ModuleSpec | None = util.spec_from_file_location(module_name, path)
+	assert spec and spec.loader
+	module: ModuleType = util.module_from_spec(spec)
+	sys.path.insert(0, str(path.parent))
+	# try:
+		# spec.loader.exec_module(module)
+	# finally:
+		# Clean up sys.path to prevent cluttering global imports
+		# sys.path.pop(0)
+	spec.loader.exec_module(module)
+	return module
+
+def load_config(module_name: str = "config", attribute: str = "CONFIG") -> Any:
+	"""
+	Loads config from $XDG_CONFIG_HOME/felt/config.py
 	Creates it if not there already.
 	"""
-	# This is where the real config lives (more declarative).
-	config_module: Path = CONFIG_PATH/"config.py"
-	config_module.parent.mkdir(exist_ok=True, parents=True)
-	if not config_module.exists():
-		with open(config_module, "w") as f:
-			f.write("")
-	spec = util.spec_from_file_location("config", config_module)
-	module: ModuleType = util.module_from_spec(spec) # type: ignore
-	assert spec and spec.loader
-	spec.loader.exec_module(module)
-	return getattr(module, "CONFIG", {})
+	module: ModuleType = import_config_module(module_name)
+	return getattr(module, attribute, None)
 
 def get_css_file() -> str:
 	style_dir: Path = CONFIG_PATH/"style"
@@ -41,19 +63,22 @@ def get_css_file() -> str:
 		with open(css, "w") as f:
 			f.write("")
 	return str(css)
+
 # This imports all modules declared in `PARAMS` dict.
 # You should be able to import your modules as if you were using
 # `from module_name import main`, where `main` is the function that will be
 # executed with the params given.
 
 def main() -> None:
-	CONFIG: dict[str, Any] = load_config()
+	CONFIG_PATH.mkdir(exist_ok=True, parents=True)
+	CONFIG: dict[str, Any] = load_config() or {}
 	app: Application = Application("shell")
 	for module_name, module_config in CONFIG.items():
-		module: ModuleType = import_module(f"{module_name}.main")
-		func: Any | None = getattr(module, "main", None)
+		func: Any | None = load_config(module_name, attribute="main")
 		if not callable(func):
 			raise ValueError(f"`from {module_name}.main import main` failed.")
 		app.add_window(func(**module_config))
 	app.set_stylesheet_from_file(get_css_file())
 	app.run()
+
+if __name__ == "__main__": main()
